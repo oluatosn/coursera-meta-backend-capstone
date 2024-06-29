99e7f288a256776a3622482955d2a8f671b2f103
oluatosn <102866085+oluatosn@users.noreply.github.com>
2024-06-29 18:33:30 +0100

    updated settings.py

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'restaurant.apps.RestaurantConfig',
+    #'restaurant.apps.RestaurantConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
